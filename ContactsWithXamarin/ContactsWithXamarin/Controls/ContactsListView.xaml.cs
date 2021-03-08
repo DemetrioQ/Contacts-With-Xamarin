b598b707a53f7ba49e1404c845c6b91343b05ec7
Demetrio Quiñones <demetriorqe@gmail.com>
2021-03-08 02:44:06 -0400

    "Better" start UI

---
+++ ContactsWithXamarin/ContactsWithXamarin/Controls/ContactsListView.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ContactsWithXamarin.Controls
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ContactsListView : ContentView
+    {
+        public ContactsListView()
+        {
+            InitializeComponent();
+        }
+    }
+}
